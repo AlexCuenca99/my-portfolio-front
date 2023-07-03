4a45402ebc8a84e62e3575dfd032650e9d7ea7d6
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-07-03 11:24:47 -0500

    Added font configuration

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,12 @@
+import { Sora } from '@next/font/google';
+const sora = Sora({
+	subsets: ['latin'],
+	variable: '--font-sora',
+	weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
+});
+
 const Layout = () => {
-  return <div>Layout</div>;
+	return <div>Layout</div>;
 };
 
 export default Layout;
