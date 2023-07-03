f1a39226986ee01ae397b206c3fad4b5ec2a8aca
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-07-03 16:51:54 -0500

    Added transition animation logic

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,22 @@
+import { useRouter } from 'next/router';
+import { AnimatePresence, motion } from 'framer-motion';
+
 import Layout from '../components/Layout';
+import Transition from '../components/Transition';
 
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+	const router = useRouter();
+
 	return (
 		<Layout>
-			<Component {...pageProps} />;
+			<AnimatePresence mode="wait">
+				<motion.div key={router.route} className="h-full">
+					<Transition />
+					<Component {...pageProps} />;
+				</motion.div>
+			</AnimatePresence>
 		</Layout>
 	);
 }
